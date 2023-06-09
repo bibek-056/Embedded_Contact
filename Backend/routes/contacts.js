b1f4216d3e28e65bd7fe85979303ac5c662d242d
bibek-056 <adh.bibek056@gmail.com>
2023-06-09 09:09:51 +0545

    Second Commit

--- Backend/routes/contacts.js
+++ Backend/routes/contacts.js
@@ -3,6 +3,7 @@ const Contact = require('../models/contactModel')
 const router = express.Router()
 const util = require('util');
 const qrCode = require('qrcode');
+const mongoose = require('mongoose')
 
 
 
@@ -52,5 +53,21 @@ router.post('/', async (req, res) => {
     }
 
 } )
+
+router.get('/:id', async ( req, res) => { 
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such Contact'})
+    }
+
+    const contact = await Contact.findById(id)
+
+    if (!contact) {
+        return res.status(404).json({error: 'No such Contact'})
+    }
+
+    res.status(200).json(contact)
+})
 
 module.exports = router;
